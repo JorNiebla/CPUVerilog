017c28e6480f31e563e5980928395e79bbb89dc8
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2022-03-06 22:54:25 +0000

    Primera mitad

--- cd.v
+++ cd.v
@@ -1,10 +1,11 @@
 module cd(input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] op_alu, output wire z, output wire [5:0] opcode);
 //Camino de datos de instrucciones de un solo ciclo
 
-    wire [15:0] INST;
-    wire [9:0] counter, outMUXDIR, DIR_SALTO;
-    wire [7:0] INM, WD3, outALU, RD1, RD2;
+    wire [31:0] INST;
+    wire [9:0] counter, outMUXDIR, DIR_SALTO, outSUMDIR;
+    wire [15:0] INM, WD3, outALU, RD1, RD2;
     wire [3:0] RA1, RA2, WA3;
+    wire zALU;
 
     assign RA1 = INST[11:8];
     assign RA2 = INST[7:4];
@@ -15,10 +16,15 @@ module cd(input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] op_alu
 
     assign opcode = INST[15:10];
 
-    registro #(WIDTH = 10) PC(clk, reset, outMUXDIR, counter);
-    memprog MemProg(clk,counter,inst);
+    registro #(10) PC(clk, reset, outMUXDIR, counter);
+    memprog MemProg(clk,counter,INST);
     sum SUMDIR(10'b1,counter,outSUMDIR);
+    mux2 #(10) MUXDIR(DIR_SALTO,outSUMDIR,s_inc,outMUXDIR);
+    
     regfile BANCO(clk,we3,RA1,RA2,WA3,WD3,RD1,RD2);
+    alu ALU(RD1,RD2,op_alu,outALU,zALU);
+    ffd FFZ(clk,reset,zALU,wez,z);
+    mux2 #(16) MUXALU(outALU,INM,s_inm,WD3);
     
 
 endmodule
--- componentes.v
+++ componentes.v
@@ -4,10 +4,10 @@
 module regfile(input  wire        clk, 
                input  wire        we3,           //señal de habilitación de escritura
                input  wire [3:0]  ra1, ra2, wa3, //direcciones de regs leidos y reg a escribir
-               input  wire [7:0]  wd3, 			 //dato a escribir
-               output wire [7:0]  rd1, rd2);     //datos leidos
+               input  wire [15:0]  wd3, 			 //dato a escribir
+               output wire [15:0]  rd1, rd2);     //datos leidos
 
-  reg [7:0] regb[0:15]; //memoria de 16 registros de 8 bits de ancho
+  reg [15:0] regb[0:15]; //memoria de 16 registros de 8 bits de ancho
 
   initial
   begin
