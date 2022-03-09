a675b9a3d941bbd35e53dfe6f1c5c57b5f98a8c6
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2022-03-09 23:03:39 +0000

    Arreglos en el camino de datos y empezada UC

--- verilog/cd.v
+++ verilog/cd.v
@@ -1,11 +1,11 @@
-module cd(input wire clk, reset, s_inc, s_inm, we3, wez, push, pop, inm, carry, input wire [2:0] op_alu, output wire z, output wire [5:0] opcode);
+module cd(input wire clk, reset, s_inc, s_inm, we3, wez, push, pop, input wire [2:0] op_alu, output wire z, carry, output wire [15:0] opcode);
 //Camino de datos de instrucciones de un solo ciclo
 
     wire [31:0] INST;
     wire [9:0] counter, outMUXDIR, DIR_SALTO, outSUMDIR, outMUXSTACK, outSTACK;
-    wire [15:0] INM, WD3, outALU, RD1, RD2, outMUXINM;
+    wire [15:0] INM, WD3, RD1, RD2, outMUXINM;
     wire [3:0] RA1, RA2, WA3;
-    wire zALU;
+    wire zALU, cALU;
 
     assign RA1 = INST[11:8];
     assign RA2 = INST[7:4];
@@ -14,18 +14,19 @@ module cd(input wire clk, reset, s_inc, s_inm, we3, wez, push, pop, inm, carry, 
     assign INM = INST[11:4];
     assign DIR_SALTO = INST[9:0];
 
-    assign opcode = INST[15:10];
+    assign opcode = INST[31:16];
 
     registro #(10) PC(clk, reset, outMUXSTACK, counter);
     memprog MemProg(clk,counter,INST);
     sum SUMDIR(10'b1,counter,outSUMDIR);
     mux2 #(10) MUXDIR(DIR_SALTO,outSUMDIR,s_inc,outMUXDIR);
     
-    mux2 #(16) MUXINM(RD1,INM[27:12],inm,outMUXINM);
+    mux2 #(16) MUXINM(RD1,INM[27:12],s_inm,outMUXINM);
     regfile BANCO(clk,we3,RA1,RA2,WA3,WD3,RD1,RD2);
-    alu ALU(outMUXINM,RD2,op_alu,outALU,zALU);
+    alu ALU(outMUXINM,RD2,op_alu,WD3,zALU, cALU);
     ffd FFZ(clk,reset,zALU,wez,z);
-    mux2 #(16) MUXALU(outALU,INM,s_inm,WD3);
+    ffd FFC(clk,reset,cALU,wez,carry);
+    //mux2 #(16) MUXALU(outALU,INM,s_inm,WD3);
     
     stack PILA(outSUMDIR, reset, push, pop, clk, outSTACK);
     mux2 #(10) MUXSTACK(outMUXDIR, outSTACK,pop, outMUXSTACK);
--- verilog/uc.v
+++ verilog/uc.v
@@ -1,9 +1,45 @@
-module uc(input wire [5:0] opcode, input wire z, output reg s_inc, s_inm, we3, wez, push, pop, inm, carry, output reg [2:0] op_alu);
+module uc(input wire [15:0] opcode, input wire z, carry, output reg s_inc, s_inm, we3, wez, push, pop, output reg [2:0] op_alu);
 
-  initial begin
+  always @(opcode)
+  begin
+    s_inm = 1'b0;
+    s_inc = 1'b1;
+    wez = 1'b0;
     we3 = 1'b0;
-
+    casez (opcode)
+      16'b101?????????????: begin
+        s_inm = 1'b1;
+        op_alu = opcode[30:28];
+        wez = 1'b1;
+        we3 = 1'b1;
+      end
+      16'b100?????????????: begin
+        s_inm = 1'b1;
+        op_alu = opcode[30:28];
+        we3 = 1'b1;
+      end
+      16'b11??????????????: begin
+        s_inm = 1'b1;
+        op_alu = opcode[30:28];
+        we3 = 1'b1;
+      end
+      16'b01???????????01?: begin
+        op_alu = opcode[18:16];
+        wez = 1'b1;
+        we3 = 1'b1;
+      end
+      16'b01???????????00?: begin
+        op_alu = opcode[18:16];
+        we3 = 1'b1;
+      end
+      16'b01???????????1??: begin
+        op_alu = opcode[18:16];
+        we3 = 1'b1;
+      end
+      16'b001?????????????: begin
+        
+      end
+    endcase
   end
-
 
 endmodule
